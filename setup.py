e4250aa93eecdc33728e690157a71bf37d5a29f9
Tim Meinhardt <meinhardt.tim@gmail.com>
2019-08-19 13:12:48 +0200

    Move setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='davis',
       packages=['davis'],
-      package_dir={'':'lib'},
+      package_dir={'':'python/lib'},
       version='0.0.1',
       install_requires=[],)
